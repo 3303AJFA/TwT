7055a35c4bec24eee9344b806da3894d53d96fdd
Ch33rsL0ve <102729849+Ch33rsL0ve@users.noreply.github.com>
2023-09-28 23:22:18 -0300

    Revert "Added player movement..."
    This reverts commit fb19db878b6faddf60b2c4e29f1499eb3ad35955.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,42 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 10.0f;
-    [SerializeField]
-    private float maxSmoothSpeed;
-    [SerializeField]
-    private Rigidbody rb;
-    private Vector3 movement;
-    
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
+        
     }
 
-    private void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        HandleMovement(movement);
-    }
-
-    private void Update()
-    {
-        movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-    }
-
-
-    private void HandleMovement(Vector3 direction)
-    {
-        rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
-
-        if (direction.magnitude >= 0.1f)
-        {
-            float Angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            float Smooth = Mathf.SmoothDampAngle(transform.eulerAngles.y, Angle, ref maxSmoothSpeed, Time.deltaTime);
-
-            transform.rotation = Quaternion.Euler(0, Smooth, 0);
-        }
+        
     }
 }
