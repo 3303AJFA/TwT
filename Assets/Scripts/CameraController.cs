1932dadf4acb426ec50023812ded0649e245ce24
Zulixo <xlebno777@gmail.com>
2023-10-03 14:54:56 +0700

    Add Walls Hiding Script...
    +add wall hiding controller script
    -remove Camera rotation with the mouse

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,36 +2,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
+    public GameObject target;
     
     [SerializeField] private float smoothSpeed;
     [SerializeField] private float movementTime;
     [SerializeField] private float keyboardRotationAmount;
-    [SerializeField] private float mouseRotationAmount;
     [SerializeField] private Vector3 offset;
     
     void FixedUpdate()
     {
         float keyboardRotateInput = Input.GetAxis("RotateCamera");
-        float mouseRotateInput = Input.GetAxis("Mouse X");
         RotateCamera(keyboardRotateInput);
-        HandleRotationInput(mouseRotateInput);
         HandleMovementInput();
         
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 desiredPosition = target.transform.position + offset;
         transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
     }
 
     private void RotateCamera(float rotationInput)
     {
-        transform.RotateAround(target.position, Vector3.up, rotationInput * keyboardRotationAmount * Time.fixedDeltaTime);
-    }
-    private void HandleRotationInput(float rotationInput)
-    {
-        if (Input.GetMouseButton(1))
-        {
-            transform.RotateAround(target.position, Vector3.up, rotationInput * mouseRotationAmount * Time.fixedDeltaTime);
-        }
+        transform.RotateAround(target.transform.position, Vector3.up, rotationInput * keyboardRotationAmount * Time.fixedDeltaTime);
     }
     
     private void HandleMovementInput()
