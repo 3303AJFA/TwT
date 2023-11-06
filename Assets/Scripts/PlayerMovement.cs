479f9ea2166d71473d4dfd5b5a556880db4509e5
pr5 <pr5@gnome.su>
2023-11-06 15:18:46 +0300

    ice now has inertion. still laggy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,46 @@
 using UnityEngine;
 
-
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerController _playerController;
-
+    private Rigidbody _rb;
+    private bool isOnIce = false;
     private float friction = 1.0f;
+    private float iceFriction = 1.2f;
     void Start()
     {
         _playerController = GetComponent<PlayerController>();
+        _rb = GetComponent<Rigidbody>();
     }
-    
- public void MovePlayer()
-    {   
-    Vector3 movement = _playerController.movement * (_playerController.speed * Time.fixedDeltaTime);
- 
-    movement *= friction;
-
-    _playerController.rb.MovePosition(_playerController.rb.position + movement);
+    public void MovePlayer()
+    {
+        Vector3 movement = _playerController.movement * (_playerController.speed * Time.fixedDeltaTime);
+        if (isOnIce)
+        {
+            movement *= iceFriction;
+            _rb.AddForce(movement, ForceMode.VelocityChange);
+        }
+        else
+        {
+            movement *= friction;
+            _rb.MovePosition(_rb.position + movement);
+        }
     }
-    private void OnCollisionEnter(Collision other) {
-	    if(other.gameObject.CompareTag("Ice")) friction = 1.5f;    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ice"))
+        {
+            isOnIce = true;
+            friction = iceFriction;
+        }
     }
-    private void OnCollisionExit(Collision other) {
-	    if(other.gameObject.CompareTag("Ice")) friction = 1.0f;    
-        if(other.gameObject.CompareTag("Ice")) print("left ice");
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ice"))
+        {
+            isOnIce = false;
+            friction = 1.0f;
+            print("left ice");
+        }
     }
 }
