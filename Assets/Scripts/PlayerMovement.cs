ef8bba81a0964360bac6aadb182a5f0607e4efe1
pr5 <pr5@gnome.su>
2023-11-06 14:55:17 +0300

    started implementing ice. thats gonna be painful.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerController _playerController;
 
+    private float friction = 1.0f;
     void Start()
     {
         _playerController = GetComponent<PlayerController>();
     }
     
-    public void MovePlayer()
-    {
-        _playerController.rb.MovePosition(_playerController.rb.position + _playerController.movement * (_playerController.speed * Time.fixedDeltaTime));
+ public void MovePlayer()
+    {   
+    Vector3 movement = _playerController.movement * (_playerController.speed * Time.fixedDeltaTime);
+ 
+    movement *= friction;
+
+    _playerController.rb.MovePosition(_playerController.rb.position + movement);
+    }
+    private void OnCollisionEnter(Collision other) {
+	    if(other.gameObject.CompareTag("Ice")) friction = 1.5f;    
+    }
+    private void OnCollisionExit(Collision other) {
+	    if(other.gameObject.CompareTag("Ice")) friction = 1.0f;    
+        if(other.gameObject.CompareTag("Ice")) print("left ice");
     }
 }
