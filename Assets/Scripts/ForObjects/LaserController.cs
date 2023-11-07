a1c16d4f2aee7682fa3d5b0700198bea7c2a4610
Zulixo <xlebno777@gmail.com>
2023-11-07 19:58:20 +0700

    Add Laser reaction to the player

--- Assets/Scripts/ForObjects/LaserController.cs
+++ Assets/Scripts/ForObjects/LaserController.cs
@@ -1,24 +1,55 @@
+using System.Collections;
 using UnityEngine;
 
 public class LaserController : MonoBehaviour
 {
     private LineRenderer _lr;
     
+    [SerializeField] private float laserForce;
+    public bool _laserHitPlayer = false;
+
     void Start()
     {
         _lr = GetComponent<LineRenderer>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
         _lr.SetPosition(0, transform.position);
         RaycastHit hit;
+        
+        Debug.DrawRay(transform.position, transform.forward, Color.yellow,5000);
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
             if (hit.collider)
             {
-                _lr.SetPosition(1, hit.point);
+                if (hit.collider.gameObject.CompareTag("Player") && !_laserHitPlayer)
+                {
+                    
+                        // Получить компонент Rigidbody игрока
+                        Rigidbody playerRigidbody = hit.collider.GetComponent<Rigidbody>();
+
+                        if (playerRigidbody != null)
+                        {
+                            // Вычислить направление от попадания лазера к центру игрока
+                            Vector3 forceDirection = playerRigidbody.transform.position - hit.point;
+
+                            // Нормализовать направление
+                            forceDirection.Normalize();
+
+                            // Приложить силу к игроку в направлении касательной от лазера
+                            playerRigidbody.AddForce(forceDirection * laserForce, ForceMode.Impulse);
+                            _laserHitPlayer = true; 
+                            Debug.Log("PLAYER TOUCH LASER");
+                            
+                        }
+                    
+                }
+                else
+                {
+                    _laserHitPlayer = false;
+                    _lr.SetPosition(1, hit.point);
+                }
             }
         }
         else _lr.SetPosition(1, transform.forward*5000);
