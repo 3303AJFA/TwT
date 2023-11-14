8a5ec990095988290591105f78fe7c6fffe5013d
Zulixo <xlebno777@gmail.com>
2023-11-14 21:21:17 +0700

    Portal Update 2

--- Assets/Scripts/ForObjects/PortalTeleport.cs
+++ Assets/Scripts/ForObjects/PortalTeleport.cs
@@ -15,6 +15,11 @@ public class PortalTeleport : MonoBehaviour
 
     private void Update()
     {
+        PlayerTeleport();
+    }
+
+    private void PlayerTeleport()
+    {
         if (playerIsOverlapping)
         {
             Vector3 portalToPlayer = player.position - transform.position;
@@ -38,23 +43,72 @@ public class PortalTeleport : MonoBehaviour
     {
         var m_collider = wall.GetComponent<Collider>();
         m_collider.isTrigger = true;
+        
         if (other.CompareTag("Player"))
         {
             playerIsOverlapping = true;
         }
+        else if (other.CompareTag("Interactive"))
+        {
+            // Сохраняем rotation объекта
+            Quaternion originalRotation = other.transform.rotation;
+            Vector3 clonePosition = new Vector3(receiver.position.x,receiver.position.y,receiver.position.z);
 
-        //_cloneObject = Instantiate(other.GameObject());
+            // Создаем клон объекта перед вторым порталом
+            _cloneObject = Instantiate(other.gameObject, clonePosition, originalRotation);
+            
+            Destroy(_cloneObject.GetComponent<Collider>());
+        }
     }
-    
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Interactive"))
+        {
+            // Определение вектора направления от объекта к порталу
+            Vector3 directionToPortal = (other.transform.position - transform.position).normalized;
+            
+            // Вычисление расстояния между объектом и порталом
+            float distance = Vector3.Distance(other.transform.position, transform.position);
+
+            // Увеличение размера клонированного объекта
+            if (_cloneObject != null)
+            {
+                _cloneObject.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                
+                // Установка новой позиции клонированного объекта относительно портала
+                _cloneObject.transform.position = receiver.position + directionToPortal * distance;
+            }
+        }
+        /*
+        if (_cloneObject != null)
+        {
+            Vector3 portalToObject = _cloneObject.transform.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToObject);
+
+            if (dotProduct < 0f)
+            {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                rotationDiff += 180;
+                _cloneObject.transform.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToObject;
+                _cloneObject.transform.position = receiver.position + positionOffset;
+            }
+        }
+        */
+    }
+
     private void OnTriggerExit(Collider other)
     {
         var m_collider = wall.GetComponent<Collider>();
         m_collider.isTrigger = false;
+        
         if (other.CompareTag("Player"))
         {
             playerIsOverlapping = false;
         }
 
-        //Destroy(_cloneObject);
+        Destroy(_cloneObject);
     }
 }
