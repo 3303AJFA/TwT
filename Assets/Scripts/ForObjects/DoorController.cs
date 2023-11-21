9e06419bbf614be2d7b84725a27a00fb17177b7b
Zulixo <xlebno777@gmail.com>
2023-11-21 23:59:33 +0700

    Add LevelLoader
    +add 2 test levels

--- Assets/Scripts/ForObjects/DoorController.cs
+++ Assets/Scripts/ForObjects/DoorController.cs
@@ -4,28 +4,31 @@ public class DoorController : MonoBehaviour
 {
     [SerializeField]private TriggerAreaController triggerAreaController;
     private Vector3 _originalPosition;
-    private float _maximumDoorTranslate = 0.3f;
-    private float _openDoorStep = 10f;
+    private float _maximumDoorTranslate = 0.8f;
+    private float _openDoorStep;
+    private float _openDoorY = 2f;
     private bool isDoorStop;
     
 
     void Start()
     {
         _originalPosition = transform.position;
+        _openDoorStep = _originalPosition.y + _openDoorY;
     }
     
     // Update is called once per frame
     void Update()
     {
-        if (triggerAreaController.door == true && _originalPosition.y < _openDoorStep && isDoorStop == false)
+        if ((triggerAreaController.door == true && transform.position.y < _openDoorStep) && isDoorStop == false)
         {
-            transform.Translate(Vector3.up * _maximumDoorTranslate * Time.deltaTime);
-            Debug.Log(transform.position + " " + _originalPosition);
+            transform.Translate(Vector3.forward * _maximumDoorTranslate * Time.deltaTime);
+            Debug.Log(transform.position + " " + _originalPosition + " " + _openDoorStep);
             isDoorStop = false;
+
         }
         else if ((triggerAreaController.door == false && transform.position.y > _originalPosition.y) && isDoorStop == false)
         {
-            transform.Translate(Vector3.down * _maximumDoorTranslate * Time.deltaTime);
+            transform.Translate(Vector3.back * _maximumDoorTranslate * Time.deltaTime);
             Debug.Log(transform.position + " " + _originalPosition);
             isDoorStop = false;
         }
